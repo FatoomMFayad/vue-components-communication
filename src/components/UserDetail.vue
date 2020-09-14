5856f9de045c518cca2ad632bcd866daa9cf8eb7
ffayad@ptcdb.edu.ps <ffayad@ptcdb.edu.ps>
2020-09-14 17:14:53 +0300

    commuincation between siblings

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,32 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name {{switchName()}}</p>
+        <p>User Age {{ userAge }}</p>
+        <button @click="resetFn">Reset Name</button>
     </div>
 </template>
 
 <script>
+    export default{
+      props: {
+         myName: {
+             type: String,
+             required: true, 
+            },
+        resetFn: Function,
+        userAge: Number
+      },
+      methods:{
+          switchName() {
+              return this.myName.split("").reverse().join("");
+          },
+          resetName() {
+              this.myName = 'Fatoom';
+              this.$emit('nameWasReset', this.myName);
+          }
+      } 
+    }
 </script>
 
 <style scoped>
